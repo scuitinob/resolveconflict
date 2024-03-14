c8180ab417e5f6f74c9d67ea05cff59529b07cb1
Sergio Cuitiño <135767213+scuitinob@users.noreply.github.com>
2024-03-13 22:22:19 -0300

    Primer Commit

---
+++ HelloJava.java
@@ -0,0 +1,12 @@
+public class HelloJava {
+	String message = "este mensaje es creado desde el repo local";
+	public static void main(String[] args) {
+
+		System.out.println("Hello Java");
+		  
+		  
+	
+
+	}
+
+}
