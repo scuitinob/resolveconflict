2959f9c124c41172c6ab1498f43158f721754d35
sergio.cuitino@avattar.com <sergio.cuitino@avattar.com>
2024-03-13 23:00:27 -0300

    dev2 commit

--- HelloJava.java
+++ HelloJava.java
@@ -1,5 +1,5 @@
 public class HelloJava {
-	String message = "este mensaje es creado desde el repo local";
+	String message = "este mensaje es creado por el dev2";
 	public static void main(String[] args) {
 
 		System.out.println("Hello Java");
